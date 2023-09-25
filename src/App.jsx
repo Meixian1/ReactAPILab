b032896bc13f15e53a0d99922d8413b32ec8752d
Applebees1 <104962202+Applebees1@users.noreply.github.com>
2023-09-25 00:19:36 -0700

    REACT

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form";
 
 export default function App() {
-  const apiKey = "YOUR_API_KEY"; // Replace with your actual API key
+  const apiKey = "98e3fb1f"; // Replace with your actual API key
   const [movie, setMovie] = useState(null);
 
   const getMovie = async (searchTerm) => {
     try {
       const response = await fetch(
-        `https://www.omdbapi.com/?apikey=${98e3fb1f}&t=${searchTerm}`
+        `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
       );
       const data = await response.json();
       setMovie(data);
